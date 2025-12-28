eddb2be83bf276dc8f1282772cb051273619e3cc
aryanseth9795 <iiitianaryan@gmail.com>
2025-12-28 16:53:10 +0530

    logo changes

--- src/services/ExportService.ts
+++ src/services/ExportService.ts
@@ -1,4 +1,4 @@
-import * as FileSystem from "expo-file-system";
+import * as FileSystem from "expo-file-system/legacy";
 import * as Sharing from "expo-sharing";
 import { zip } from "react-native-zip-archive";
 import {
